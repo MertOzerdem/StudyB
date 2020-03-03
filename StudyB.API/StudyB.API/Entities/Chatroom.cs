737ab9d402f6e40025de665b916539fbd1b3a413
MertOzerdem <32201738+MertOzerdem@users.noreply.github.com>
2020-03-03 16:56:59 +0300

    API version 1.0

---
+++ StudyB.API/StudyB.API/Entities/Chatroom.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudyB.API.Entities
+{
+    public class Chatroom
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public String ChatroomName { get; set; }
+        public ICollection<Message> Messages { get; set; }
+            = new List<Message>();
+        public ICollection<UserChatroom> UserChatrooms { get; set; }
+            = new List<UserChatroom>();
+    }
+}
---
+++ StudyB.API/StudyB.API/Entities/Reward.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudyB.API.Entities
+{
+    public class Reward
+    {
+        [Key]
+        public int Id { get; set; }
+
+        // Change later do not seem like a fit here rewards should not keep users that have them
+        [ForeignKey("UserId")]
+        public User User { get; set; }
+        public Guid UserId { get; set; }
+    }
+}
