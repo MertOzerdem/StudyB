a053443eae3cb76db3987787362e9f006a083644
MertOzerdem <32201738+MertOzerdem@users.noreply.github.com>
2020-03-03 22:42:56 +0300

    v1.1

---
+++ StudyB.API/StudyB.API/Services/IStudyRepository.cs
@@ -0,0 +1,14 @@
+﻿using StudyB.API.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudyB.API.Services
+{
+    interface IStudyRepository
+    {
+        User GetUser(Guid Id);
+        IEnumerable<User> GetUsers();
+    }
+}
---
+++ StudyB.API/StudyB.API/Services/StudyRepository.cs
@@ -0,0 +1,50 @@
+﻿using StudyB.API.DbContexts;
+using StudyB.API.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudyB.API.Services
+{
+    public class StudyRepository : IDisposable, IStudyRepository
+    {
+        private readonly BuddyLibraryContext context;
+
+        public StudyRepository(BuddyLibraryContext context)
+        {
+            this.context = context;
+        }
+
+        public IEnumerable<User> GetUsers()
+        {
+            return this.context.Users.ToList<User>();
+        }
+
+        public User GetUser(Guid Id)
+        {
+            if (Id == Guid.Empty)
+            {
+                throw new ArgumentNullException(nameof(Id));
+            }
+
+            return this.context.Users.Where(u => u.Id == Id).FirstOrDefault();
+        }
+
+
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                // dispose resources when needed
+            }
+        }
+    }
+}
